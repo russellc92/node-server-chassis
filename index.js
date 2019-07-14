6ea166c569fe8145444ecd86d642765406b59070
russellc92 <russell_claxton@hotmail.com>
2019-07-14 17:21:10 +0100

    added express

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-console.log('hello-world')
+app.use(bodyParser.json());
 
-console.log('env: ', process.env.PORT || 3000)
+app.get('/', async (req, res) => {
+    console.log('Incoming request: ', req);
+
+    return res
+        .status(200)
+        .send('hello world');
+});
+
+const port = process.env.DEP_ENV === 'production' ? (process.env.PORT || 80) : 3000;
+
+const server = app.listen(port, function () {
+    console.log('Server started on port: ', port);
+})
